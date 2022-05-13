84ff440e763f33a03d1de2735042228806eddc6c
geraldo.tech <geraldo.filho92@hotmail.com>
2022-05-12 21:14:10 -0300

    fetch date

---
+++ fetch-git.js
@@ -0,0 +1,8 @@
+async function pegardadosgit(){
+return await fetch('https://api.github.com/users/geraldotech');
+}
+console.log('[teste]', pegardadosgit());
+pegardadosgit()
+.then((valordoretorno) => {
+
+})
