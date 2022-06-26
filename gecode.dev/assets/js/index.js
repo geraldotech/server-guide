87b2524a8b37e722436434796996a35c78138403
geraldo.tech <geraldo.filho92@hotmail.com>
2022-06-26 12:29:34 -0300

    a

---
+++ gecode.dev/assets/js/index.js
@@ -0,0 +1,10 @@
+var app = document.getElementById("app");
+var p = document.createElement("p");
+var img = document.createElement("img");
+//const url = ;
+p.classList.add("p1");
+p.textContent = "Geraldo";
+img.setAttribute("src", "".concat("http://smartdicastutorial.appspot.com/images/6d88733c461.jpg"));
+img.classList.add("myimg");
+app === null || app === void 0 ? void 0 : app.appendChild(p);
+app === null || app === void 0 ? void 0 : app.appendChild(img);
