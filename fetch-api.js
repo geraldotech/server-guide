63a989f485e0c0c93d173140f94239280eaad3b6
geraldo.tech <geraldo.filho92@hotmail.com>
2023-02-20 08:09:09 -0300

    :zap:

--- fetch-api.js
+++ fetch-api.js
@@ -1,32 +1,31 @@
-
 // api url
-const api_url =
-	"https://api.github.com/users/geraldotech";
+const api_url = "https://api.github.com/users/geraldotech";
 
 // Defining async function
 async function getapi(url) {
-	
-	// Storing response
-	const response = await fetch(url);
-	
-	// Storing data in form of JSON
-	var data = await response.json();
-	console.log(data);
-	if (response) {
-	}
+  // Storing response
+  const response = await fetch(url);
+
+  // Storing data in form of JSON
+  var data = await response.json();
+  console.log(data);
+  if (response) {
+  }
   show(data);
 }
 // Calling that async function
 getapi(api_url);
 
-
-function show(data){
-  console.log(data.login)
+function show(data) {
+  console.log(data.login);
   document.getElementById("root").innerHTML = data.name;
   document.getElementById("img").innerHTML = `<img src="${data.avatar_url}" />`;
   document.getElementById("bio").innerHTML = `${data.bio}`;
-  console.log(data.bio)
-  
+  console.log(data.bio);
 }
 
-console.log('git commit');
+console.log("git commit");
+
+function hello(name) {
+  console.log(name);
+}
